b49cf82cf94abbc18218bbf4c8bdf6043a8f59a7
walikhan1 <walikhan@live.unc.edu>
2019-08-26 23:12:31 -0400

    A1 Novice

---
+++ src/a1/src/A1Novice.java
@@ -0,0 +1,62 @@
+
+
+import java.util.Scanner;
+
+public class A1Novice {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		// Your code follows here.
+		
+	int numCust = scan.nextInt();
+	
+	String [] fullName = new String  [numCust]; 
+		
+	int [] totPrice = new int[numCust];
+		
+	for (int i=0; i< numCust; i++) {
+	
+		String firstName = scan.next();
+		
+		char initial = firstName.charAt(0);
+	
+		String lastName = scan.next();
+		
+		int numItems = scan.nextInt(); 
+		
+		fullName [i] = initial + "." + lastName;
+		 
+	
+	for (int j=0; j< numItems; j++) {
+	
+		int quan = scan.nextInt();
+	
+		String items = scan.next();
+	
+		double price = scan.nextDouble();
+	
+		totPrice[j] += quan * price;	
+		
+		
+	}
+	
+
+	
+	}
+	
+	scan.close();
+	for (int k=0; k< numCust; k++) {
+	
+	
+	System.out.println(fullName [k] + ": "+ totPrice[k]);
+	
+	
+
+	
+
+	}		
+		
+	}
+}
